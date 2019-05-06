1519b8427797d7128efb29bbd75774bb00cfaa72
Martin Balk <m@martinbalk.com>
2019-05-06 09:38:21 +0200

    Add comments to subscribable contract

--- contracts/SLA/Subscribable.sol
+++ contracts/SLA/Subscribable.sol
@@ -3,22 +3,44 @@ pragma solidity 0.5.7;
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "../Whitelist/Whitelist.sol";
 
+/**
+ * @title Subscribable
+ * @dev Subscribable adds the functionality to subscribe to a contract
+ */
 contract Subscribable {
 
     using SafeMath for uint256;
 
+    // Address of the whitelist contract
     Whitelist public whitelist;
 
+    // Mapping to return if a user address is subscribed
     mapping(address => bool) private userToSubscribed;
 
+    // Array with all users that are subsribed or have been subscribed
     address[] internal allUsers;
 
+    // Total amount of subscribers
     uint public subscribersCount;
+
+    // Total amount of users that are subscribed or have been subscribed
     uint internal usersCount;
 
+    /**
+     * @dev event for logging users subscribing
+     * @param user the address of the subscribed user
+     */
     event Subscribed(address indexed user);
+
+    /**
+     * @dev event for logging users unsubscribing
+     * @param user the address of the unsubscribed user
+     */
     event Unsubscribed(address indexed user);
 
+    /**
+     * @dev Throws if sender is not whitelisted when whitelist is present
+     */
     modifier onlyWhitelisted() {
         if (whitelist != Whitelist(0)) {
             require(whitelist.isWhitelisted(msg.sender));
@@ -26,16 +48,25 @@ contract Subscribable {
         _;
     }
 
+    /**
+     * @dev Throws if sender is not subscribed
+     */
     modifier onlySubscribed() {
         require(isSubscribed(msg.sender));
         _;
     }
 
+    /**
+     * @dev Throws if sender is subscribed
+     */
     modifier onlyNotSubscribed() {
         require(!isSubscribed(msg.sender));
         _;
     }
 
+    /**
+     * @dev internal function for subscribing to the contract
+     */
     function _subscribe() internal onlyWhitelisted {
         userToSubscribed[msg.sender] = true;
         subscribersCount = subscribersCount.add(1);
@@ -45,6 +76,9 @@ contract Subscribable {
         emit Subscribed(msg.sender);
     }
 
+    /**
+     * @dev internal function for unsubscribing to the contract
+     */
     function _unSubscribe() internal onlySubscribed {
         userToSubscribed[msg.sender] = false;
         subscribersCount = subscribersCount.sub(1);
@@ -52,6 +86,12 @@ contract Subscribable {
         emit Unsubscribed(msg.sender);
     }
 
+    /**
+     * @dev public view funtion to check if a given user is subscribed to the
+     * contract
+     * @param _userAddress the address of the user to check
+     * @return true if _userAddress is subscribed to the contract
+     */
     function isSubscribed(address _userAddress) public view returns(bool) {
         return(userToSubscribed[_userAddress]);
     }
