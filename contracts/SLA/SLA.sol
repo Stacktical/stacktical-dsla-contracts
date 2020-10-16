788fbe259a913ccc43ccd338fe446fd8cebc277f
Yosra Helal <helal.yosra@gmail.com>
2020-10-16 13:49:35 +0200

    deleting whitelist contracts

--- contracts/SLA/SLA.sol
+++ contracts/SLA/SLA.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "../interfaces/IMessenger.sol";
-import "../Whitelist/Whitelist.sol";
 import "../SLO/SLO.sol";
 import "../SLARegistry.sol";
 import "./Staking.sol";
