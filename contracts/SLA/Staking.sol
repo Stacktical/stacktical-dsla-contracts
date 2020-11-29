0e6ec710806de549b32e87d0b0ae8eb324942cfa
Matías Barrios <matias.barriosn@gmail.com>
2020-11-29 15:18:25 -0300

    chore: pragma updated

--- contracts/SLA/Staking.sol
+++ contracts/SLA/Staking.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.6;
+pragma solidity ^0.6.0;
 
 import "../bDSLA/bDSLAToken.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
--- contracts/bDSLA/bDSLAToken.sol
+++ contracts/bDSLA/bDSLAToken.sol
@@ -1,6 +1,6 @@
 // bDSLAToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity 0.6.6;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
