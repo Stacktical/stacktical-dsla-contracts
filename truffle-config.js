601e005d61111b3dace8440e8848da640a6faf42
Matías Barrios <matias.barriosn@gmail.com>
2020-11-29 15:14:23 -0300

    chore: artifacts updated

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,3 @@
-require("dotenv").config();
-const HDWalletProvider = require("truffle-hdwallet-provider");
-
-const infura_project_id = process.env.INFURA_PROJECT_ID;
-const mnemonic_dev = process.env.DSLA_MNEMONIC_DEV;
-
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -67,7 +61,7 @@ module.exports = {
       provider: function () {
         return new HDWalletProvider(
           mnemonic_dev,
-          "https://ropsten.infura.io/v3/" + infura_project_id
+          "https://rinkeby.infura.io/v3/" + infura_project_id
         );
       },
       network_id: "3",
@@ -83,28 +77,6 @@ module.exports = {
       network_id: "42",
       gas: 12000000,
     },
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    development: {
-      host: "127.0.0.1", // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: "*", // Any network (default: none)
-    },
-    // Another network with more advanced options...
-    // advanced: {
-    // port: 8777,             // Custom port
-    // network_id: 1342,       // Custom network
-    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-    // from: <address>,        // Account to send txs from (default: accounts[0])
-    // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
     ropsten: {
       provider: () =>
         new HDWalletProvider(
@@ -116,13 +88,7 @@ module.exports = {
       confirmations: 2, // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200, // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true, // Skip dry run before migrations? (default: false for public nets )
-    },
-    // Useful for private networks
-    // private: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-    // network_id: 2111,   // This network is yours, in the cloud.
-    // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    }
   },
 
   // Set default mocha options here, use special reporters etc.
