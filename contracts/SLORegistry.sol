b50e2b27920f5d7a02c06d1f93a3f80c6d728c89
Matías Barrios <matias.barriosn@gmail.com>
2020-12-23 10:01:34 -0300

    feat: SLORegistry contract NATSpec comments

--- contracts/SLORegistry.sol
+++ contracts/SLORegistry.sol
@@ -9,14 +9,15 @@ import "./SLO/SLO.sol";
  * objectives and querying those service level objectives
  */
 contract SLORegistry {
-    // Mapping that stores the service level objectives created by a user
+    /// @dev mapping that stores the service level objectives created by a user
     mapping(address => SLO[]) private userToSLOs;
 
     /**
      * @dev event for service level objective creation logging
-     * @param slo The address of the created service level objective contract
+     * @param slo 1.  the address of the created service level objective contract
+     * @param owner 2.  the creator of the SLO
      */
-    event SLOCreated(SLO indexed slo);
+    event SLOCreated(SLO indexed slo, address indexed owner);
 
     /**
      * @dev public function for creating service level objectives
@@ -32,7 +33,7 @@ contract SLORegistry {
         SLO slo = new SLO(_value, _SLOType, _name);
         userToSLOs[msg.sender].push(slo);
 
-        emit SLOCreated(slo);
+        emit SLOCreated(slo, msg.sender);
     }
 
     /**
@@ -40,6 +41,7 @@ contract SLORegistry {
      * the given user has created
      * @param _user Address of the user for which to return the service level
      * objectives
+     * @return SLO a service level objective that corresponds to the _user params
      */
     function userSLOs(address _user) public view returns (SLO[] memory) {
         return (userToSLOs[_user]);
