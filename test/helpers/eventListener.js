c125c780929d920388c8d96f5a44714b65222de4
Matías Barrios <matias.barriosn@gmail.com>
2020-12-23 12:06:59 -0300

    chore: sliEvent to print events

--- test/helpers/eventListener.js
+++ test/helpers/eventListener.js
@@ -14,19 +14,42 @@ const contractCreator = (contract) => {
   }
 };
 
+const filterEventValues = (values) => {
+  const valuesCount = Object.keys(values).length;
+  const splicedEntries = Object.entries(values).slice(
+    valuesCount / 2,
+    valuesCount
+  );
+  return splicedEntries.reduce((r, [k, v]) => {
+    return Object.assign(r, { [k]: v });
+  }, {});
+};
+
 // event listener
 export const eventListener = async (contract, event) => {
   const web3Contract = contractCreator(contract);
   return new Promise((resolve, reject) => {
-    web3Contract.once(
-      event,
+    web3Contract.events.allEvents(
       {
         fromBlock: "latest",
       },
       (error, result) => {
-        if (error) return reject(error);
-        return resolve(result);
+        if (error) {
+          return reject(error);
+        }
+        const { event: name, returnValues: values } = result;
+        const response = { name, values: filterEventValues(values) };
+        if (process.env.NODE_ENV === "test") {
+          console.log(response);
+        }
+        if (result.event === event) {
+          return resolve(response);
+        }
       }
     );
+    // .on("data", () => (error, result) => {
+    //   if (error) return reject(error);
+    //   return resolve(result);
+    // });
   });
 };
