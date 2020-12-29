890b80db7e9af418cf05fd285f3be2d1936fb07f
Matías Barrios <matias.barriosn@gmail.com>
2020-12-29 09:13:59 -0300

    fix: DAI contract name updated

--- contracts/testing/DAI.sol
+++ contracts/testing/DAI.sol
@@ -20,7 +20,7 @@ import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
  * and pauser roles to other accounts.
  */
 
-contract bDSLAToken is ERC20PresetMinterPauser {
+contract DAI is ERC20PresetMinterPauser {
     mapping (address => uint256) private _allowedBalances;
     mapping (address => bool) private _claimed;
     event Allowed(uint indexed _amount, address indexed _claimer);
