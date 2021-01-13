f085be078ba0179adfe286bc6807ddf30e9e8716
Matías Barrios <matias.barriosn@gmail.com>
2021-01-13 10:45:05 -0300

    feat: stake dai script conditionally for Kovan

--- scripts/stake-dai.js
+++ scripts/stake-dai.js
@@ -2,18 +2,21 @@ const DAI = artifacts.require('DAI');
 const SLA = artifacts.require('SLA');
 const { toWei, fromWei } = web3.utils;
 
-const SLAAdress = '0x439aEa5362e99C77c1C345ED8AA08E0b09D10F05';
+const SLAAddress = '0x3caAf58FEf79B0F61D858bD2F13904Ea5776c907';
 
 module.exports = async (callback) => {
   try {
-    const sla = await SLA.at(SLAAdress);
-
+    const sla = await SLA.at(SLAAddress);
     const [owner, notOwner] = await web3.eth.getAccounts();
     const account = process.env.NOT_OWNER ? notOwner : owner;
 
     console.log(`Owner address is: ${account}`);
 
-    const dai = await DAI.deployed();
+    const networkType = await web3.eth.net.getNetworkType();
+    const dai = networkType === 'kovan'
+      ? await DAI.at('0x4f96fe3b7a6cf9725f59d353f723c1bdb64ca6aa')
+      : await DAI.deployed();
+
     console.log(`DAI address is: ${dai.address}`);
     const daiAlreadyAdded = await sla.allowedTokensMapping.call(dai.address);
     console.log(`DAI added status is: ${daiAlreadyAdded}`);
@@ -21,10 +24,10 @@ module.exports = async (callback) => {
       await sla.addAllowedTokens(dai.address);
     }
 
-    const stakeAmount = toWei(String('10000'));
+    const stakeAmount = toWei(String('10'));
     const balance = await dai.balanceOf(owner);
     console.log(`Owner balance is: ${balance}`);
-    await dai.approve(SLAAdress, stakeAmount, { from: account });
+    await dai.approve(SLAAddress, stakeAmount, { from: account });
     await sla.stakeTokens(stakeAmount, dai.address, 4, { from: account });
     const daiStakingIndex = await sla.userStakedTokensIndex.call(
       account,
