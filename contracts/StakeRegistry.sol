f48bf03262b39aa6bb080ffdbf4d8e4a445bf318
Matías Barrios <matias.barriosn@gmail.com>
2021-02-19 13:28:06 -0300

    feat: active pool from stakeRegistry

--- contracts/StakeRegistry.sol
+++ contracts/StakeRegistry.sol
@@ -1,7 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "./SLA.sol";
+import "./SLARegistry.sol";
 
 /**
  * @title StakeRegistry
@@ -33,6 +37,9 @@ contract StakeRegistry is Ownable {
     uint256 private _minimumDSLAStakedTier3 = 9000 ether;
     /// @dev array with the allowed tokens addresses of the StakeRegistry
     address[] public allowedTokens;
+
+    /// @dev (userAddress => SLA[]) with user staked SLAs to get tokenPool
+    mapping(address => SLA[]) public userStakedSlas;
 
     /**
      * @dev event to log modifications on the staking parameters
@@ -90,63 +97,101 @@ contract StakeRegistry is Ownable {
         minimumDSLAStakedTier3 = _minimumDSLAStakedTier3;
     }
 
-    //    /**
-    //     * @dev returns the active pools owned by a user.
-    //     * @param _slaOwner 1. owner of the active pool
-    //     * @return ActivePool[], array of structs: {SLAAddress,stake,assetName}
-    //     */
-    //    function getActivePool(address _slaOwner)
-    //        public
-    //        view
-    //        returns (ActivePool[] memory)
-    //    {
-    //        bytes4 NAME_SELECTOR = bytes4(keccak256(bytes("name()")));
-    //        uint256 stakeCounter = 0;
-    //        // Count the stakes of the user, checking every SLA staked
-    //        for (
-    //            uint256 index = 0;
-    //            index < userStakedSlas[_slaOwner].length;
-    //            index++
-    //        ) {
-    //            SLA currentSLA = SLA(userStakedSlas[_slaOwner][index]);
-    //            stakeCounter = stakeCounter.add(
-    //                currentSLA.getAllowedTokensLength()
-    //            );
-    //        }
-    //
-    //        ActivePool[] memory activePools = new ActivePool[](stakeCounter);
-    //        // to insert on activePools array
-    //        uint256 stakePosition = 0;
-    //        for (
-    //            uint256 index = 0;
-    //            index < userStakedSlas[_slaOwner].length;
-    //            index++
-    //        ) {
-    //            SLA currentSLA = userStakedSlas[_slaOwner][index];
-    //            for (
-    //                uint256 tokenIndex = 0;
-    //                tokenIndex < currentSLA.getAllowedTokensLength();
-    //                tokenIndex++
-    //            ) {
-    //                (address tokenAddress, uint256 stake) =
-    //                    currentSLA.getTokenStake(_slaOwner, tokenIndex);
-    //                (, bytes memory tokenNameBytes) =
-    //                    tokenAddress.staticcall(
-    //                        abi.encodeWithSelector(NAME_SELECTOR)
-    //                    );
-    //                ActivePool memory currentActivePool =
-    //                    ActivePool({
-    //                        SLAAddress: address(currentSLA),
-    //                        stake: stake,
-    //                        assetName: string(tokenNameBytes),
-    //                        assetAddress: tokenAddress
-    //                    });
-    //                activePools[stakePosition] = currentActivePool;
-    //                stakePosition = stakePosition.add(1);
-    //            }
-    //        }
-    //        return activePools;
-    //    }
+    /**
+     *@dev public view function that returns true if the _owner has staked on _sla
+     *@param _user 1. address to check
+     *@param _sla 2. sla to check
+     *@return bool, true if _sla was staked by _user
+     */
+    function slaWasStakedByUser(address _user, address _sla)
+        public
+        view
+        returns (bool)
+    {
+        for (uint256 index = 0; index < userStakedSlas[_user].length; index++) {
+            if (address(userStakedSlas[_user][index]) == _sla) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *@dev register the sending SLA contract as staked by _owner
+     *@param _owner 1. SLA contract to stake
+     */
+    function registerStakedSla(address _owner, address _slaRegistry)
+        public
+        returns (bool)
+    {
+        SLARegistry slaRegistry = SLARegistry(_slaRegistry);
+        require(
+            slaRegistry.isRegisteredSLA(msg.sender) == true,
+            "Only for registered SLAs"
+        );
+        if (slaWasStakedByUser(_owner, msg.sender) == false) {
+            userStakedSlas[_owner].push(SLA(msg.sender));
+        }
+        return true;
+    }
+
+    /**
+     * @dev returns the active pools owned by a user.
+     * @param _slaOwner 1. owner of the active pool
+     * @return ActivePool[], array of structs: {SLAAddress,stake,assetName}
+     */
+    function getActivePool(address _slaOwner)
+        public
+        view
+        returns (ActivePool[] memory)
+    {
+        bytes4 NAME_SELECTOR = bytes4(keccak256(bytes("name()")));
+        uint256 stakeCounter = 0;
+        // Count the stakes of the user, checking every SLA staked
+        for (
+            uint256 index = 0;
+            index < userStakedSlas[_slaOwner].length;
+            index++
+        ) {
+            SLA currentSLA = SLA(userStakedSlas[_slaOwner][index]);
+            stakeCounter = stakeCounter.add(
+                currentSLA.getAllowedTokensLength()
+            );
+        }
+
+        ActivePool[] memory activePools = new ActivePool[](stakeCounter);
+        // to insert on activePools array
+        uint256 stakePosition = 0;
+        for (
+            uint256 index = 0;
+            index < userStakedSlas[_slaOwner].length;
+            index++
+        ) {
+            SLA currentSLA = userStakedSlas[_slaOwner][index];
+            for (
+                uint256 tokenIndex = 0;
+                tokenIndex < currentSLA.getAllowedTokensLength();
+                tokenIndex++
+            ) {
+                (address tokenAddress, uint256 stake) =
+                    currentSLA.getTokenStake(_slaOwner, tokenIndex);
+                (, bytes memory tokenNameBytes) =
+                    tokenAddress.staticcall(
+                        abi.encodeWithSelector(NAME_SELECTOR)
+                    );
+                ActivePool memory currentActivePool =
+                    ActivePool({
+                        SLAAddress: address(currentSLA),
+                        stake: stake,
+                        assetName: string(tokenNameBytes),
+                        assetAddress: tokenAddress
+                    });
+                activePools[stakePosition] = currentActivePool;
+                stakePosition = stakePosition.add(1);
+            }
+        }
+        return activePools;
+    }
 
     //_______ OnlyOwner functions _______
     function setStakingParameters(
--- test/stakeRegistry.test.js
+++ test/stakeRegistry.test.js
@@ -1,101 +1,192 @@
-// const { toWei, utf8ToHex } = web3.utils;
+import { expect } from 'chai';
+import { cleanSolidityString } from './helpers';
+import { networkNames, networkNamesBytes32, networks } from '../constants';
+import getIPFSHash from './helpers/getIPFSHash';
 
-// const initialTokenSupply = '100';
-// const stakeAmount1 = toWei(String(initialTokenSupply / 10));
-// const stakeAmount2 = toWei(String(initialTokenSupply / 5));
+const bDSLA = artifacts.require('bDSLA');
+const DAI = artifacts.require('DAI');
+const USDC = artifacts.require('USDC');
+const SLA = artifacts.require('SLA');
+const SLARegistry = artifacts.require('SLARegistry');
+const SLORegistry = artifacts.require('SLORegistry');
+const StakeRegistry = artifacts.require('StakeRegistry');
+const SEMessenger = artifacts.require('SEMessenger');
 
-// it('should ask for active pool correctly', async () => {
-//   const [sla] = SLAs;
-//   await sla.addAllowedTokens(dai.address);
-//
-//   // with owner
-//   await bDSLA.approve(sla.address, stakeAmount1);
-//   await dai.approve(sla.address, stakeAmount2);
-//   await sla.stakeTokens(stakeAmount1, bDSLA.address, periodId);
-//   await sla.stakeTokens(stakeAmount2, dai.address, periodId);
-//
-//   const slaStakedByOwner = await slaRegistry.slaWasStakedByUser(
-//     owner,
-//     sla.address,
-//   );
-//   // eslint-disable-next-line no-unused-expressions
-//   expect(slaStakedByOwner).to.be.true;
-//
-//   let activePools = await slaRegistry.getActivePool.call(owner);
-//   assert.equal(
-//     activePools.length,
-//     2,
-//     'active pools should only be equal to SLAs length',
-//   );
-//
-//   let [pool1, pool2] = activePools;
-//   let bDSLAName = await bDSLA.name.call();
-//   let daiName = await dai.name.call();
-//
-//   assert.equal(pool1.stake, stakeAmount1, 'stakes for SLA 1 does not match');
-//   assert.equal(pool2.stake, stakeAmount2, 'stakes for SLA 2 does not match');
-//   assert.equal(
-//     cleanSolidityString(pool1.assetName),
-//     bDSLAName,
-//     'names for SLA 1 does not match',
-//   );
-//   assert.equal(
-//     cleanSolidityString(pool2.assetName),
-//     daiName,
-//     'names for SLA 2 does not match',
-//   );
-//   assert.equal(
-//     pool1.SLAaddress,
-//     sla.address,
-//     'addresses for pool 1 does not match',
-//   );
-//   assert.equal(
-//     pool2.SLAaddress,
-//     sla.address,
-//     'addresses for pool 2 does not match',
-//   );
-//
-//   // with notOwner
-//   await bDSLA.approve(sla.address, stakeAmount1, { from: notOwner });
-//   await dai.approve(sla.address, stakeAmount2, { from: notOwner });
-//   await sla.stakeTokens(stakeAmount1, bDSLA.address, periodId, {
-//     from: notOwner,
-//   });
-//   await sla.stakeTokens(stakeAmount2, dai.address, periodId, {
-//     from: notOwner,
-//   });
-//
-//   // for notOwner
-//   activePools = await slaRegistry.getActivePool.call(notOwner);
-//   assert.equal(
-//     activePools.length,
-//     2,
-//     'active pools should only be equal to SLAs length',
-//   );
-//   [pool1, pool2] = activePools;
-//   bDSLAName = await bDSLA.name.call({ from: notOwner });
-//   daiName = await dai.name.call({ from: notOwner });
-//
-//   assert.equal(pool1.stake, stakeAmount1, 'stakes for SLA 1 does not match');
-//   assert.equal(pool2.stake, stakeAmount2, 'stakes for SLA 2 does not match');
-//   assert.equal(
-//     cleanSolidityString(pool1.assetName),
-//     bDSLAName,
-//     'names for SLA 1 does not match',
-//   );
-//   assert.equal(
-//     cleanSolidityString(pool2.assetName),
-//     daiName,
-//     'names for SLA 2 does not match',
-//   );
-//   assert.equal(
-//     pool1.SLAaddress,
-//     sla.address,
-//     'addresses for pool 1 does not match',
-//   );
-//   assert.equal(
-//     pool2.SLAaddress,
-//     sla.address,
-//     'addresses for pool 2 does not match',
-//   );
-// });
+const { toWei } = web3.utils;
+
+const initialTokenSupply = '100';
+const stakeAmount1 = toWei(String(initialTokenSupply / 10));
+const stakeAmount2 = toWei(String(initialTokenSupply / 5));
+
+const sloValue = 95000;
+const sloType = 4;
+
+// 2 is Weekly SLA
+const periodType = 2;
+const slaNetwork = networkNames[0];
+const slaNetworkBytes32 = networkNamesBytes32[0];
+
+describe('StakeRegistry', () => {
+  // Addresses
+  let owner;
+  let notOwner;
+  // Tokens
+  let bdsla;
+  let dai;
+  let usdc;
+  let seMessenger;
+  let stakeRegistry;
+  let slaRegistry;
+  let slo;
+  let sla;
+  let ipfsHash;
+
+  before(async () => {
+    [owner, notOwner] = await web3.eth.getAccounts();
+    bdsla = await bDSLA.deployed();
+    await bdsla.mint(owner, toWei(initialTokenSupply));
+    usdc = await USDC.deployed(); // to simulate a new token
+    await usdc.mint(owner, toWei(initialTokenSupply));
+    dai = await DAI.deployed();
+    await dai.mint(owner, toWei(initialTokenSupply));
+
+    // mint to notOwner
+    await bdsla.mint(notOwner, toWei(initialTokenSupply));
+    await usdc.mint(notOwner, toWei(initialTokenSupply));
+    await dai.mint(notOwner, toWei(initialTokenSupply));
+
+    const serviceMetadata = {
+      serviceName: networks[slaNetwork].validators[0],
+      serviceDescription: 'Official DSLA Beta Partner.',
+      serviceImage:
+        'https://storage.googleapis.com/dsla-incentivized-beta/validators/chainode.svg',
+      serviceURL: 'https://dsla.network',
+      serviceAddress: 'one18hum2avunkz3u448lftwmk7wr88qswdlfvvrdm',
+      serviceTicker: slaNetwork,
+    };
+
+    ipfsHash = await getIPFSHash(serviceMetadata);
+    const sloRegistry = await SLORegistry.deployed();
+    // 4 is "GreatherThan"
+    slo = await sloRegistry.sloAddresses.call(sloValue, sloType);
+
+    stakeRegistry = await StakeRegistry.deployed();
+    await stakeRegistry.addAllowedTokens(dai.address);
+    await stakeRegistry.addAllowedTokens(usdc.address);
+
+    seMessenger = await SEMessenger.deployed();
+    slaRegistry = await SLARegistry.deployed();
+  });
+
+  beforeEach(async () => {
+    // Register the SLA
+
+    await slaRegistry.createSLA(
+      slo,
+      ipfsHash,
+      periodType,
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+      seMessenger.address,
+      false,
+      [slaNetworkBytes32],
+    );
+
+    const slaAddresses = await slaRegistry.userSLAs(owner);
+    sla = await SLA.at(slaAddresses[slaAddresses.length - 1]);
+  });
+
+  it('should ask for active pool correctly', async () => {
+    await sla.addAllowedTokens(dai.address);
+
+    // with owner
+    await bdsla.approve(sla.address, stakeAmount1);
+    await dai.approve(sla.address, stakeAmount2);
+    await sla.stakeTokens(stakeAmount1, bdsla.address);
+    await sla.stakeTokens(stakeAmount2, dai.address);
+
+    const slaStakedByOwner = await stakeRegistry.slaWasStakedByUser(
+      owner,
+      sla.address,
+    );
+    // eslint-disable-next-line no-unused-expressions
+    expect(slaStakedByOwner).to.be.true;
+
+    let activePools = await stakeRegistry.getActivePool.call(owner);
+    assert.equal(
+      activePools.length,
+      2,
+      'active pools should only be equal to SLAs length',
+    );
+
+    let [pool1, pool2] = activePools;
+    let bDSLAName = await bdsla.name.call();
+    let daiName = await dai.name.call();
+
+    assert.equal(pool1.stake, stakeAmount1, 'stakes for SLA 1 does not match');
+    assert.equal(pool2.stake, stakeAmount2, 'stakes for SLA 2 does not match');
+    assert.equal(
+      cleanSolidityString(pool1.assetName),
+      bDSLAName,
+      'names for SLA 1 does not match',
+    );
+    assert.equal(
+      cleanSolidityString(pool2.assetName),
+      daiName,
+      'names for SLA 2 does not match',
+    );
+    assert.equal(
+      pool1.SLAAddress,
+      sla.address,
+      'addresses for pool 1 does not match',
+    );
+    assert.equal(
+      pool2.SLAAddress,
+      sla.address,
+      'addresses for pool 2 does not match',
+    );
+
+    // with notOwner
+    await bdsla.approve(sla.address, stakeAmount1, { from: notOwner });
+    await dai.approve(sla.address, stakeAmount2, { from: notOwner });
+    await sla.stakeTokens(stakeAmount1, bdsla.address, {
+      from: notOwner,
+    });
+    await sla.stakeTokens(stakeAmount2, dai.address, {
+      from: notOwner,
+    });
+
+    // for notOwner
+    activePools = await stakeRegistry.getActivePool.call(notOwner);
+    assert.equal(
+      activePools.length,
+      2,
+      'active pools should only be equal to SLAs length',
+    );
+    [pool1, pool2] = activePools;
+    bDSLAName = await bdsla.name.call({ from: notOwner });
+    daiName = await dai.name.call({ from: notOwner });
+
+    assert.equal(pool1.stake, stakeAmount1, 'stakes for SLA 1 does not match');
+    assert.equal(pool2.stake, stakeAmount2, 'stakes for SLA 2 does not match');
+    assert.equal(
+      cleanSolidityString(pool1.assetName),
+      bDSLAName,
+      'names for SLA 1 does not match',
+    );
+    assert.equal(
+      cleanSolidityString(pool2.assetName),
+      daiName,
+      'names for SLA 2 does not match',
+    );
+    assert.equal(
+      pool1.SLAAddress,
+      sla.address,
+      'addresses for pool 1 does not match',
+    );
+    assert.equal(
+      pool2.SLAAddress,
+      sla.address,
+      'addresses for pool 2 does not match',
+    );
+  });
+});
