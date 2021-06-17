92fa9514a82a06ab40361fe95213b1f7ef89d535
Matías Barrios <matias.barriosn@gmail.com>
2021-06-16 22:55:46 -0400

    feat: deploy fixture complete

--- deploy/0_deploy_dsla_protocol.ts
+++ deploy/0_deploy_dsla_protocol.ts
@@ -8,13 +8,47 @@ module.exports = async ({
   network,
 }: HardhatRuntimeEnvironment) => {
   const { deployer } = await getNamedAccounts();
-  const { deploy } = deployments;
+  const { deploy, get } = deployments;
   const baseOptions: DeployOptionsBase = {
     from: deployer,
     log: true,
   };
 
+  await deploy(CONTRACT_NAMES.StringUtils, baseOptions);
+  await deploy(CONTRACT_NAMES.PeriodRegistry, baseOptions);
+  await deploy(CONTRACT_NAMES.SLORegistry, baseOptions);
   await deploy(CONTRACT_NAMES.MessengerRegistry, baseOptions);
+  await deploy(CONTRACT_NAMES.DSLA, {
+    ...baseOptions,
+    contract: CONTRACT_NAMES.ERC20,
+    args: [CONTRACT_NAMES.DSLA, CONTRACT_NAMES.DSLA],
+  });
+  const { address: dslaTokenAddress } = await get(CONTRACT_NAMES.DSLA);
+
+  await deploy(CONTRACT_NAMES.StakeRegistry, {
+    ...baseOptions,
+    args: [dslaTokenAddress],
+  });
+  const periodRegistry = await get(CONTRACT_NAMES.PeriodRegistry);
+  const sloRegistry = await get(CONTRACT_NAMES.SLORegistry);
+  const messengerRegistry = await get(CONTRACT_NAMES.MessengerRegistry);
+  const stakeRegistry = await get(CONTRACT_NAMES.StakeRegistry);
+  const stringUtils = await get(CONTRACT_NAMES.StringUtils);
+  const checkPastPeriods = false;
+  await deploy(CONTRACT_NAMES.SLARegistry, {
+    ...baseOptions,
+    ...(network.config.gas !== 'auto' && { gasLimit: network.config.gas }),
+    args: [
+      sloRegistry.address,
+      periodRegistry.address,
+      messengerRegistry.address,
+      stakeRegistry.address,
+      checkPastPeriods,
+    ],
+    libraries: {
+      StringUtils: stringUtils.address,
+    },
+  });
 };
 
 module.exports.tags = [DEPLOYMENT_TAGS.DSLA];
