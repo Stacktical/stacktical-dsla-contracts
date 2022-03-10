3f1d79046c77d4c2e11a7a5fe99a1f7c1c237d0c
Wilhem Pujar <wilhem.pujar@stacktical.com>
2022-03-10 23:18:42 +0100

    feat: rewardPecentage equals deviation

--- contracts/SLA.sol
+++ contracts/SLA.sol
@@ -130,20 +130,14 @@ contract SLA is Staking {
             precision
         );
 
-        uint256 normalizedPeriodId = _periodId.sub(initialPeriodId).add(1);
-
-        uint256 rewardPercentage = deviation.mul(normalizedPeriodId).div(
-            finalPeriodId - initialPeriodId + 1
-        );
-
         if (_sloRegistry.isRespected(sliValue, address(this))) {
             periodSLI.status = Status.Respected;
 
-            _setRespectedPeriodReward(_periodId, rewardPercentage, precision);
+            _setProviderReward(_periodId, deviation, precision);
         } else {
             periodSLI.status = Status.NotRespected;
 
-            _setUsersCompensation(_periodId, rewardPercentage, precision);
+            _setUserReward(_periodId, deviation, precision);
         }
     }
 
