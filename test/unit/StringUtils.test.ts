1aead8ff814423717834e38731e35dec9ebaff2d
gmtesla <g.topman09@gmail.com>
2022-10-19 22:45:50 +0800

    feat: update code coverage up to 98%

--- test/unit/StringUtils.test.ts
+++ test/unit/StringUtils.test.ts
@@ -1,12 +1,16 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { utils } from "ethers";
+import { BigNumber, utils } from "ethers";
 import { ethers } from "hardhat";
 import { CONTRACT_NAMES } from "../../constants";
 import { MockStringUtils } from "../../typechain";
 
 describe(CONTRACT_NAMES.StringUtils, () => {
+	let user1: SignerWithAddress;
 	let stringUtils: MockStringUtils;
+
 	before(async () => {
+		[user1] = await ethers.getSigners();
 		const MockStringUtils = await ethers.getContractFactory("MockStringUtils");
 		stringUtils = <MockStringUtils>await MockStringUtils.deploy();
 		await stringUtils.deployed();
@@ -21,6 +25,40 @@ describe(CONTRACT_NAMES.StringUtils, () => {
 			const value = 3.1415926535897932384626 // 22 decimals
 			const uint = await stringUtils.stringFloatToUnit(utils.formatBytes32String(value.toString()));
 			expect(utils.formatUnits(uint, 18)).to.be.equal('3.141592653589793');
+		})
+	})
+	describe("addressToString", () => {
+		it("should convert address to string", async () => {
+			expect(
+				await stringUtils.addressToString(user1.address)
+			).to.be.equal(user1.address.toLowerCase());
+		})
+	})
+	describe("bytes32ToStr", () => {
+		it("should convert bytes to string", async () => {
+			const value="DSLA Core";
+			expect(
+				await stringUtils.bytes32ToStr(utils.formatBytes32String(value))
+			).to.be.equal(value);
+		})
+	})
+	describe("bytesToUint", () => {
+		it("should convert bytes to string", async () => {
+			const value="123";
+			expect(
+				await stringUtils.bytesToUint(utils.formatBytes32String(value))
+			).to.be.equal(BigNumber.from(value));
+		})
+	})
+	describe("uintToStr", () => {
+		it("should convert bytes to string", async () => {
+			const value=1234;
+			expect(
+				await stringUtils.uintToStr(value)
+			).to.be.equal(value.toString());
+			expect(
+				await stringUtils.uintToStr(0)
+			).to.be.equal("0");
 		})
 	})
 })
